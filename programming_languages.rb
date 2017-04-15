cdb3b0da8193124069c5fc2a89f5bed7016f371e
David Chase <david.chase4@gmail.com>
2017-04-15 13:06:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,25 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  oo_array = []
+  functional_array = []
+  both_array = []
+  languages.each do |style, style_data|
+    if style == :oo
+      oo_array << style
+      both_array << style
+        style_data.each do |language, language_data|
+          new_hash[language] = {type: language_data[:type], style: oo_array}
+        end
+    elsif style == :functional
+      functional_array << style
+      both_array << style
+        style_data.each do |language, language_data|
+          new_hash[language] = {type: language_data[:type], style: functional_array}
+          if language == :javascript
+            new_hash[language] = {type: language_data[:type], style: both_array}
+          end
+        end
+    end
+  end
+  new_hash
 end
